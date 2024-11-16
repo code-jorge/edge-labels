b4e30b13d3f7945bdfe8e9ddb76138676dfd6c2f
Jorge Aguirre <jorge.aguirre.leon@gmail.com>
2024-11-16 14:20:07 +0100

    Small fixes

--- edge/app.js
+++ edge/app.js
@@ -1,11 +1,29 @@
 const OFFSET_WIDTH = 12;
 const LETTER_WIDTH = 8;
 
+const getDetails = (path) => {
+  try {
+    const [bg, fg, content] = path.substring(1).split("/", 3);
+    if (!bg || !fg || !content) throw new Error("MISSING PARAMS");
+    // Check bg - Should be a valid hex color
+    if (!/^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(bg)) throw new Error("INVALID BG COLOR");
+    // Check fg - Should be a valid hex color
+    if (!/^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(fg)) throw new Error("INVALID FG COLOR");
+    // Check text - Should be letters, numbers and spacebars, also special spanish letters
+    const raw_text = decodeURIComponent(content);
+    if (!/^[A-Za-z0-9ÁÉÍÓÚáéíóúÑñÜü ]+$/.test(raw_text)) throw new Error("INVALID TEXT");
+    const text = raw_text.toUpperCase();
+    return [`#${bg}`, `#${fg}`, text];
+  } catch (e) {
+    return ['red', 'white', e.message || 'BAD REQUEST'];
+  }
+}
+
 export default (request) => {
   const url = new URL(request.url);
   const path = url.pathname;
-  const [bg, fg, content] = path.substring(1).split("/", 3);
-  const text = content.toUpperCase();
+  if (path === "/") return;
+  const [bg, fg, text] = getDetails(path);
   const wordWidth = text.length * LETTER_WIDTH;
   const pathEnd = wordWidth + OFFSET_WIDTH;
   const totalWidth = pathEnd + OFFSET_WIDTH;
